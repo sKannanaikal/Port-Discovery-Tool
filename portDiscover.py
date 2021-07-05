1cc00dc87dbdfd8233a0c1752651d337a5987742
sKannanaikal <stk5106@rit.edu>
2021-07-05 18:03:44 -0400

    created the main program and added threading compatability

--- portDiscover.py
+++ portDiscover.py
@@ -10,33 +10,33 @@ class portScanner:
 		self.target = target
 		self.open_ports = []
 		self.threads = []
-		for i in range(self.reqPortScan):
-			threading.Thread(target=scan, args=(i)).start()
+		for i in range(1, self.reqPortScan):
+			threading.Thread(target=scan, args=(i, self)).start()
 
-	def scan(port_num):
+	def displayResults(self):
+		print('''After scanning all {requested_ports} requested ports, {open_port_count} port(s) have been found to be open on {target_machine}'''.format(
+		requested_ports=self.reqPortScan, 
+		open_port_count=len(self.open_ports), 
+		target_machine=self.target))
+		print(self.open_ports)
+
+def scan(port_num, scanner):
 		portConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
-			portConnection.connect(self.target, port_num)
+			portConnection.connect((scanner.target, port_num))
 			print('[+] Port: {port} is open!'.format(port=port_num))
-			self.open_ports.append(port_num)
+			scanner.open_ports.append(port_num)
 		except socket.error:
 			print('[-] Port: {port} is not open!'.format(port=port_num))
 
-	def displayResults():
-		print('''After scanning all {requested_ports} requested ports, {open_port_count} 
-		port(s) have been found to be open on {target_machine}'''.format(requested_ports=self.reqPortScan, 
-		open_port_count=len(self.open_ports), target_machine=self.target))
-		print(open_ports)
-
 def main():
 	command = optparse.OptionParser()
-	command.add_option('-t', action=store, dest='target', type='string',help='identify target machine that you would like to port scan')
-	command.add_option('-c'm action=store, dest='port_count', type='int', help='identify how many of the ports you would like scanned starting from 1')
+	command.add_option('-t', action='store', dest='target', type='string',help='identify target machine that you would like to port scan')
+	command.add_option('-c', action='store', dest='port_count', type='int', help='identify how many of the ports you would like scanned starting from 1')
 	options, args = command.parse_args()
 	reqPortScan = options.port_count
 	target = options.target
 	scanner = portScanner(reqPortScan, target)
-	scanner.run()
 
 if __name__ == '__main__':
 	main()
